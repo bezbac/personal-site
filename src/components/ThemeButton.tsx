d77c9e638124d25c4359069a4f1fe0c381f64f32
Ben Bachem <10088265+bezbac@users.noreply.github.com>
2025-04-19 11:21:05 +0200

    Add icons for light and dark mode buttons

--- src/components/ThemeButton.tsx
+++ src/components/ThemeButton.tsx
@@ -1,12 +1,20 @@
 import type React from "react";
+import { useCallback, useEffect, useState } from "react";
+
+const determineCurrentMode = () => {
+  return document.documentElement.classList.contains("dark") ? "dark" : "light";
+};
 
 export default function ThemeButton(
   props: React.PropsWithChildren<{
     mode: "light" | "dark";
     className?: string;
+    "aria-label"?: string;
   }>
 ) {
-  const handleClick = () => {
+  const [currentMode, setCurrentMode] = useState<"light" | "dark">();
+
+  const handleClick = useCallback(() => {
     if (props.mode === "dark") {
       document.documentElement.classList.remove("light");
       document.documentElement.classList.add("dark");
@@ -18,10 +26,30 @@ export default function ThemeButton(
     }
 
     localStorage.setItem("theme", props.mode);
-  };
+  }, []);
+
+  useEffect(() => {
+    const observer = new MutationObserver(() => {
+      setCurrentMode(determineCurrentMode());
+    });
+
+    observer.observe(document.documentElement, {
+      attributes: true,
+      attributeFilter: ["class"],
+    });
+
+    setCurrentMode(determineCurrentMode());
+
+    return () => observer.disconnect();
+  }, [setCurrentMode]);
 
   return (
-    <button className={props.className} onClick={handleClick}>
+    <button
+      className={props.className}
+      onClick={handleClick}
+      aria-selected={currentMode === props.mode}
+      aria-label={props["aria-label"]}
+    >
       {props.children}
     </button>
   );
