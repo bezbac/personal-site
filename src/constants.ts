21c708da95cae72c14c8089036b18af3cde67b2f
Ben Bachem <10088265+bezbac@users.noreply.github.com>
2026-09-12 14:48:26 +0200

    Extract social links and update bluesky profile url

---
+++ src/constants.ts
@@ -0,0 +1,4 @@
+export const BLUESKY_URL = "https://bsky.app/profile/bezbac.dev";
+export const LINKED_IN_URL = "https://www.linkedin.com/in/ben-bachem";
+export const GITHUB_URL = "https://github.com/bezbac";
+export const EMAIL = "hello@benbachem.de";
